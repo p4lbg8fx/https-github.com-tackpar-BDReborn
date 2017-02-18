c0e2b76fbe0aef0a12c46afd3623b0627fedad80
yaser1382 <yaser.13821382@gmail.com>
2017-02-18 15:51:05 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -71,7 +71,7 @@ function create_config( )
     "plugins",
     "tools"
  },
-    sudo_users = {157059515},
+    sudo_users = {188213002},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
